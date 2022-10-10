e58521d81ad0144c3f5ab7f407d4aa497ceb594c
Matheus Santos <matheussantos77@outlook.com>
2022-10-10 15:47:31 -0300

    initial redux configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
+
+import { store } from './store';
+
 import MyRoutes from './routes/routes';
 
 import Header from './components/Header';
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <MyRoutes />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <MyRoutes />
+      </Router>
+    </Provider>
   );
 }
 
